23d672c1bd0b97b745f7e0d60533928d549a575f
Xheldon <c1006044256@gmail.com>
2022-11-18 16:18:08 +0800

    Initial Commit

---
+++ swift-learn/swift_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_learnApp.swift
+//  swift-learn
+//
+//  Created by caoxudong on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct swift_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
